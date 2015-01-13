a019acd68c8747024840dbda0d5ff992492852b2
贺鹏飞 <hepengfei@xunlei.com>
2015-01-13 18:18:00 +0800

    增加了导入菜单项

--- app/src/main/java/io/github/hepengfei/choujiang/PoolManagerActivity.java
+++ app/src/main/java/io/github/hepengfei/choujiang/PoolManagerActivity.java
@@ -56,7 +56,7 @@ public class PoolManagerActivity extends ActionBarActivity {
         int id = item.getItemId();
 
         //noinspection SimplifiableIfStatement
-        if (id == R.id.action_settings) {
+        if (id == R.id.action_import) {
             return true;
         }
 
